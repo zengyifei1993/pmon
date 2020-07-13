530a6664f2b4cb591f4f014d1fbb249efe982253
Chong Qiao <qiaochong@loongson.cn>
2020-07-13 14:30:53 +0800

    boot_initrd decaled in initrd.h, because long long param.
    Change-Id: I4885e1fe728257398e33f8d1d59f80f63a5cebe1
    Signed-off-by: Chong Qiao <qiaochong@loongson.cn>

--- include/initrd.h
+++ include/initrd.h
@@ -5,6 +5,7 @@
 int initrd_execed(void);
 unsigned long long get_initrd_start(void);
 unsigned int get_initrd_size(void);
+int boot_initrd(const char* path, long long rdstart,int flags);
 #ifndef INITRD_ADDR
 #define INITRD_ADDR (int)0x84000000
 #endif
