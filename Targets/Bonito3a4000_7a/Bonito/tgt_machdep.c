zengyifei1993/pmon

commit a460dc524062d324e712ba00ca9d5dc48f469475

Remove RTC enable. Configure RTC and APCI low power mode.
If it is necessary to read and write the 0x50 / 0x54 register of ACPI.
The bit [9] of the 0x8 register of ACPI needs to be configured to be 0.

Change-Id: Ib112a5e13580dca7a7ab675b2e908f86c352e317

Targets/Bonito3a4000_7a/Bonito/tgt_machdep.c
@@ -288,6 +288,7 @@ void initmips(unsigned int raw_memsz)
 	 *  Probe clock frequencys so delays will work properly.
 	 */
 	ls7a_pwm(0, 10000);
+	init_acpi();
 	tgt_cpufreq();
 	SBD_DISPLAY("DONE",0);
 	/*
@@ -664,11 +665,20 @@ static void ls7a_pwm(int x,int y)
 	readl(LS7A_PWM3_CTRL) |= 1;
 }
 
+static void init_acpi(void)
+{
+	if (ls7a_version ()) {
+		do {
+			readl(LS7A_ACPI_PMCON_RTC_REG) |= (0x3 << 9);
+		} while (((readl(LS7A_ACPI_PMCON_RTC_REG) >> 9) & 0x3) != 0x3);
+	}
+}
+
 static void init_legacy_rtc(void)
 {
 	int year, month, date, hour, min, sec, val;
-	val = (1 << 13) | (1 << 11) | (1 << 8);
-	
+	val = (1 << 11) | (1 << 8);
+
 	outl(LS7A_RTC_CTRL_REG, val);
 	outl(LS7A_TOY_TRIM_REG, 0);
 	outl(LS7A_RTC_TRIM_REG, 0);
@@ -677,10 +687,10 @@ static void init_legacy_rtc(void)
 
 	year = inl(LS7A_TOY_READ1_REG);
 	month = (val >> 26) & 0x3f;
-	date = (val >> 21) & 0x1f; 
-	hour = (val >> 16) & 0x1f; 
-	min = (val >> 10) & 0x3f; 
-	sec = (val >> 4) & 0x3f; 
+	date = (val >> 21) & 0x1f;
+	hour = (val >> 16) & 0x1f;
+	min = (val >> 10) & 0x3f;
+	sec = (val >> 4) & 0x3f;
 	if ((year < 0 || year > 138)
 		|| (month < 1 || month > 12)
 		|| (date < 1 || date > 31)

Targets/Bonito3a4000_7a/include/ls7a.h
@@ -21,6 +21,7 @@
 #define LS7A_RTC_MATCH2_REG                             (LS7A_RTC_REG_BASE + 0x0074)
 
 #define LS7A_ACPI_REG_BASE                              (LS7A_MISC_BASE + 0x50000)
+#define LS7A_ACPI_PMCON_RTC_REG                         (LS7A_ACPI_REG_BASE + 0x8)
 #define LS7A_ACPI_PM1_STS_REG                           (LS7A_ACPI_REG_BASE + 0xc)
 #define LS7A_ACPI_PM1_CNT_REG                           (LS7A_ACPI_REG_BASE + 0x14)
 #define LS7A_ACPI_RST_CNT_REG                           (LS7A_ACPI_REG_BASE + 0x30)

commit 928953ec468f9ac2f42dcf109105e7d8e46719c9

Fix compilation error, missing declaration, commit a460dc524062d324e712ba00ca9d5dc48f469475 caused
Change-Id: I0abe250243e0753f828e561b521c2e78cc2ecc2d

Targets/Bonito3a4000_7a/Bonito/tgt_machdep.c
@@ -149,6 +149,7 @@ void ad_err1(unsigned long *adr1, unsigned long *adr2, unsigned long good, unsig
 void mv_error(unsigned long *adr, unsigned long good, unsigned long bad);
 
 static void init_legacy_rtc(void);
+static void init_acpi(void);
 
 #ifdef INTERFACE_3A780E
 #define REV_ROW_LINE 560